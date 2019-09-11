a6b6604223c68b04fbc8a923d41dba1fe359e119
Emmanuel Mathot <emmanuel.mathot@gmail.com>
2019-09-11 14:26:09 +0200

    workflow
    Conflicts:
    	src/main/app-resources/application.xml

--- src/main/app-resources/node_forecast/run.R
+++ src/main/app-resources/node_forecast/run.R
@@ -81,10 +81,10 @@ while(length(input <- readLines(stdin_f, n=1)) > 0) {
         rciop.log("INFO", paste("Processing input:", input, sep=" "))
     
         # Download the file
-        res <- rciop.copy(input, TMPDIR, uncompress=TRUE)
+        model_file <- rciop.copy(input, TMPDIR, uncompress=TRUE)
     
-        if (res$exit.code==0) {
-            local.url <- res$output
+        if (model_file$exit.code==0) {
+            local.url <- model_file$output
         }
     
         #local.url)
