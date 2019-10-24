2b238309e352217dfe2106efcdc650293fbcfc59
Tobias Lagander <tobias.lagander@smhi.se>
2019-10-24 08:45:27 +0200

    Redo test of installation on other sandbox.

--- src/main/app-resources/scripts/postinstall.sh
+++ src/main/app-resources/scripts/postinstall.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
 
+#
+# Add R libraries/packages and other necessary SW components to the develop and run-time environment.
+# Prefereably, install the main part of the major SW components first via yum to reduce any
+# incompability that may arise when conda upgrades/downgrades or installs additional library
+# dependecies. And not all libraries/packages are available from the conda channels.
+# If more SW components have been added, also add the installation dependency to pom.xml and the
+# user guide for setting up a sandbox environment.
+#
+
 #/opt/anaconda/bin/conda install -y --file /application/dependencies/R/packages.list
 /opt/anaconda/bin/conda install -y --file /application/dependencies/R/new_attempt_packages.list
 
-/opt/anaconda/bin/conda create --name cairo-env --file /application/dependencies/R/cairo-env.list
-/opt/anaconda/bin/conda create --name cdo-env --file /application/dependencies/R/cdo-env.list
+# Now when postprocessing (maps, trigger etc) moved to fanfar-postprocessing.git we shouldn't need
+# to change version of library/package jpeg
+#/opt/anaconda/bin/conda create --name cairo-env --file /application/dependencies/R/cairo-env.list
+
+# Disabled due to variant 2 of "netcdf to obs"
+#/opt/anaconda/bin/conda create --name cdo-env --file /application/dependencies/R/cdo-env.list
