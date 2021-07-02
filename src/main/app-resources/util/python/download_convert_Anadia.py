e304fac3afd57e19125d560439109f1a6c7a662b
Tobias Lagander <tobias.lagander@smhi.se>
2021-07-02 10:21:00 +0200

    Update script

--- src/main/app-resources/util/python/download_convert_Anadia.py
+++ src/main/app-resources/util/python/download_convert_Anadia.py
@@ -14,9 +14,12 @@ import time
 # Script that convert file from Anadia dataformat to HTEP format
 # Libraries: pip3 install --user dbfread
 
-# NOTE: We assume all stations have timezone UTC+2
+# NOTE: We assume all stations have timezone UTC+2  (Info from Leandro 30 Sep 2020: "our stations use UTC + 1 time zone." --> IS THIS A BUG BELOW?
 
+script_ver="v4"
 # 20200910 Tobias: v2, H-TEP run-time adaptions, Changed Depth to WaterLevel
+# 20210121 Tobias: v3, Niamey added.
+# 20210630 Tobias: v4, Duplicates.
 
 
 def search_subid_stationid_linkage_file(subid_stationid_file,name_or_id):
@@ -41,7 +44,7 @@ def parse_data(url, name, subid_stationid_file, year):
     lat = str(row["Latitude"])
     lon = str(row["Longitude"])
     UoM = ["cm", "m3/s"]
-    datatype = ["WaterLevel", "Discharge"] #"Depth"
+    datatype = ["WaterLevel", "Discharge"]
 
     counter = 0
     
@@ -61,8 +64,10 @@ def parse_data(url, name, subid_stationid_file, year):
 
         
     for x in range(0, len(datatype)):
+        # download data, url contains station and year, latest time step first
         with urllib.request.urlopen(url) as response:
             data = response.read().decode('utf-8')
+            #ToDo: Write data to dir original, remove use of curl
         
             for line in reversed(data.split("\n")):
 
@@ -72,15 +77,28 @@ def parse_data(url, name, subid_stationid_file, year):
                 if "date" in line or "Date" in line:
                     continue
 
-                #Id,Name,Timestamp,Latitude,Longitude,Uom,Value,Type
-                # date, hour, depth, battery 
-                date = line.split(",")[0]
-                hour = line.split(",")[1]
-                value = line.split(",")[2+x]
+                # date, hour, depth, Q
+                date = line.split(",")[0]    # 2020-01-08
+                hour = line.split(",")[1]    # 07:00
+                value = line.split(",")[2+x] # -999.0, 0.0 or float value
+
+                # if not str(year) in date:
+                #     print("INFO: Downloaded data contains incorrect year",date)
+                #     continue
 
                 if int(float(value)) == -999:
+                    # Skip line for water level and discharge
                     continue
 
+                # if x == 0 and value == "0.0":
+                #     # Skip line for water level
+                #     continue
+                
+                # if x == 1 and value == "0.0":
+                #     # Skip line for discharge
+                #     continue
+
+                #Id,Name,Timestamp,Latitude,Longitude,Uom,Value,Type
                 outfile[x].write(str(counter) + "," + name + "," + date + "T" + hour + ":00.0000000+01:00" + "," + lat + "," + lon + "," + UoM[x] + "," + value + "," + datatype[x] + "\n")
                 
                 counter += 1
@@ -107,6 +125,79 @@ def combine_datatypes(subid_stationid_linkage_file, url):
     subprocess.call("rm -rf " + "./htep_format/" + name + ".csv_Q", shell=True)
     subprocess.call("rm -rf " + "./htep_format/" + name + ".csv_Q2", shell=True)
 
+# Check for duplicate timestamps per data type, keep the last found
+# Update existing file in dir htep_format
+# Assumes csv file timestamps are ordered
+# Sequential ids' are not updated after removal of duplicates
+def check_remove_duplicate_timestamps(subid_stationid_linkage_file, url,verbose=False):
+    row = search_subid_stationid_linkage_file(subid_stationid_linkage_file,url)
+    stn_id = row["StationId"]
+
+    import csv
+
+    curr_file="htep_format/" + stn_id + ".csv"
+    if not os.path.isfile(curr_file):
+        print("file missing, abort",curr_file)
+        return(1)
+        
+    data_in = []
+    with open(curr_file,"r",) as csv_file:
+        reader = csv.reader(csv_file,delimiter=',')
+        for row in reader:
+            data_in.append(row)
+            # data_in[0]
+            #     ['Id', 'Name', 'Timestamp', 'Latitude', 'Longitude', 'Uom', 'Value', 'Type']
+            # data_in[1]
+            #     ['0', 'NE-P001-NIAMEY-A', '2020-01-01T00:00:00.0000000+01:00', '13.502', '2.1038', 'cm', '-999.0', 'WaterLevel']
+            # data_in[-1]
+            #     ['21721', 'NE-P001-NIAMEY-A', '2021-06-23T00:00:00.0000000+01:00', '13.502', '2.1038', 'm3/s', '-999.0', 'Discharge']
+
+    if len(data_in) > 0:
+        # Rename file extension, to not trigger other codes pattern match for .csv
+        org_file="htep_format/" + "filtered-" + stn_id + ".txt"
+        with open(org_file,"w",) as csv_file:
+            writer = csv.writer(csv_file,delimiter=',')
+            writer.writerows(data_in)
+
+    data_out = data_in
+    
+    col_timestamp = 2
+    col_value = 6
+    col_type = 7
+    last_row = len(data_in)
+    #data_in[0][col_timestamp]
+    #data_in[0][col_value]
+    #data_in[0][col_type]
+
+    list_row_indexes_to_remove = []
+    if last_row > 0:
+        for r1 in range(0,last_row - 1):
+            for r2 in range(r1,last_row):
+                if r1 != r2:
+                    if data_in[r1][col_timestamp] == data_in[r2][col_timestamp]:
+                        if data_in[r1][col_type] == data_in[r2][col_type]:
+                            if verbose:
+                                print(r1,r2,data_in[r1][col_timestamp],data_in[r1][col_type],data_in[r1][col_value],data_in[r2][col_value])
+                                print('remove row from data_out',r1,data_in[r1])
+                                print('keep row from data_out',r2,data_in[r2])
+                            list_row_indexes_to_remove.append(r1)
+                            #check remaining    break # for r2
+   
+    if len(list_row_indexes_to_remove) > 0:
+        row_indexes_removed = 0
+        for r1 in range(0, len(list_row_indexes_to_remove)):
+            if verbose:
+                print("remove",r1, list_row_indexes_to_remove[r1],len(data_out))
+            del data_out[list_row_indexes_to_remove[r1] - row_indexes_removed]
+            row_indexes_removed += 1
+
+        if len(data_out) > 0:
+            with open(curr_file,"w",) as csv_file:
+                writer = csv.writer(csv_file,delimiter=',')
+                writer.writerows(data_out)
+    
+    return(0)
+
 
 def clean():
     now = time.time()
@@ -116,7 +207,7 @@ def clean():
             if "01.tar" not in f:
                 os.remove(os.path.join("./original/", f))
 
-def main(subid_stationid_linkage_file,output_dir):
+def main(subid_stationid_linkage_file,output_dir,backup_raw_data=True,check_duplicates=True):
     # Main function
 
     # If not exists, create output dir and sub-dirs
@@ -140,16 +231,30 @@ def main(subid_stationid_linkage_file,output_dir):
     subprocess.check_call("rm -rf htep_format/*", shell=True)
 
     urls = {}
-    urls["Garbey Kourou"] = "http://slapis.fi.ibimet.cnr.it:8080/SlapisWS/api/data/j_get_stations_data/csv/1/GarbeyKourou/"
-    urls["Bossey Bangou"] = "http://slapis.fi.ibimet.cnr.it:8080/SlapisWS/api/data/j_get_stations_data/csv/2/BosseyBangou/"
+    urls["Garbey Kourou"]    = "http://slapis.fi.ibimet.cnr.it:8080/SlapisWS/api/data/j_get_stations_data/csv/1/GarbeyKourou/"
+    urls["Bossey Bangou"]    = "http://slapis.fi.ibimet.cnr.it:8080/SlapisWS/api/data/j_get_stations_data/csv/2/BosseyBangou/"
+    #urls["Larba Touloumbo"]  = "http://slapis.fi.ibimet.cnr.it:8080/SlapisWS/api/data/j_get_stations_data/csv/3/LarbaTouloumbo/"
+    #urls["Larba Birno"]      = "http://slapis.fi.ibimet.cnr.it:8080/SlapisWS/api/data/j_get_stations_data/csv/4/LarbaBirno/"
+    #urls["Garbey Kourou"]    = "http://slapis.fi.ibimet.cnr.it:8080/SlapisWS/api/data/j_get_stations_data/csv/5/GarbeyKourou/" # Garbey Kourou [1.607889,13.738068]
+    #urls["Talle"]            = "http://slapis.fi.ibimet.cnr.it:8080/SlapisWS/api/data/j_get_stations_data/csv/6/Talle/" # TallÃ© / Tallé
+    #urls["Toure"]            = "http://slapis.fi.ibimet.cnr.it:8080/SlapisWS/api/data/j_get_stations_data/csv/7/Toure/" # TourÃ© / Touré
+    urls["Niamey"]           = "http://slapis.fi.ibimet.cnr.it:8080/SlapisWS/api/data/j_get_stations_data/csv/8/Niamey/"
 
     # + datetime.today().strftime('%Y-%m-%d') 
 
+    if backup_raw_data:
+        for url in urls:
+            subprocess.check_call("rm -rf original/" + urls[url].split("/")[-2] + ".txt", shell=True)
+            for year in range(2019, (datetime.now().year)+1):
+                # The download of data via curl is not used when parsing data and creating output file in htep format
+                subprocess.call("curl " + "'" + urls[url] + str(year) + "' > original/" + urls[url].split("/")[-2] + "_" + str(year) + ".txt", shell=True) # Use separate yearly files instead of append
+        subprocess.check_call("tar cf original/data_" + datetime.today().strftime('%Y-%m-%d') + ".tar original/*.txt", shell=True)
+    
     failed_input_files = []
     for url in urls:
-        subprocess.check_call("rm -rf original/" + urls[url].split("/")[-2] + ".txt", shell=True)
         for year in range(2019, (datetime.now().year)+1):
-            subprocess.call("curl " + "'" + urls[url] + str(year) + "' >> original/" + urls[url].split("/")[-2] + ".txt", shell=True) 
+            print(url,year)
+            # Download the same raw data via python urllib, create or append yearly data to separate waterlevel and discharge files for each station
             ret = parse_data(urls[url] + str(year), url, subid_stationid_linkage_file, year)
             time.sleep(2)
             
@@ -157,10 +262,11 @@ def main(subid_stationid_linkage_file,output_dir):
                 failed_input_files.append(urls[url])
                 break
 
+        # Combine the separate yearly waterlevel and discharge files in dir htep_format
         combine_datatypes(subid_stationid_linkage_file, url)
+        if check_duplicates:
+            check_remove_duplicate_timestamps(subid_stationid_linkage_file, url)
                          
-    subprocess.check_call("tar cf original/data_" + datetime.today().strftime('%Y-%m-%d') + ".tar original/*.txt", shell=True)
-
     if len(failed_input_files) > 0:
         # Save failed_convertions into a file
         with open ("./htep_format/failed_convertions.txt", "w") as w:
@@ -171,13 +277,14 @@ def main(subid_stationid_linkage_file,output_dir):
 if __name__ == "__main__":
 
     run_env = os.getenv('_CIOP_APPLICATION_PATH',default='local')
+    print('version',script_ver)
     print(run_env)
+    
     if run_env == 'local':
         from dbfread import DBF
         subid_stationid_linkage_file = "/data/proj9/Fouh/Global/Projekt/FANFAR/Model/WestAfricaHYPE_v1.0/Gauges&Subids/SUBID-StationID-linkage_WWH_1.3.7_20200813.dbf"
         output_dir                   = "/data/proj9/Fouh/Global/Projekt/FANFAR/Data/Sirba-Anadia/dataFROMAnadia/auto_down"
-        #output_dir                   = "/var/tmp/Data/Sirba-Anadia/dataFROMAnadia/auto_down"
-
+        backup_raw_data_to_dir_original = True # Separate download via curl to backup downloaded data in original format
     else:
         # H-TEP
         import argparse
@@ -200,6 +307,8 @@ if __name__ == "__main__":
         if args.dbfread_path:
             module_dbfread_path = args.dbfread_path
 
+        backup_raw_data_to_dir_original = False
+        
         if subid_stationid_linkage_file == None or output_dir == None or module_dbfread_path == None:
             print("Path to subid stationid linkage file missing or output dir or module dbfread not defined")
             print("./download_convert_Anadia.py -i <path+filename> -o <path + name of output dir>")
@@ -211,4 +320,4 @@ if __name__ == "__main__":
         sys.path.append(module_dbfread_path)
         from dbfread import DBF
 
-    main(subid_stationid_linkage_file,output_dir)
+    main(subid_stationid_linkage_file,output_dir,backup_raw_data_to_dir_original)
